e15d2d80f38f17c1f4e79632d8b4875b138302ef
Thibaud <creatibofficiel@gmail.com>
2025-04-13 13:58:58 +0200

    Add TypeORM integration to CharacterVariantsModule
    - Imported TypeOrmModule and configured it to use CharacterVariant entity.
    - Updated module exports to include TypeOrmModule and CharacterVariantsService for better modularity.

--- src/character-variants/character-variants.module.ts
+++ src/character-variants/character-variants.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CharacterVariantsService } from './character-variants.service';
 import { CharacterVariantsController } from './character-variants.controller';
+import { CharacterVariant } from './character-variant.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CharacterVariant])],
   controllers: [CharacterVariantsController],
   providers: [CharacterVariantsService],
+  exports: [CharacterVariantsService, TypeOrmModule],
 })
 export class CharacterVariantsModule {}
