5b79d2982b27c02420fbb7a5000b2c95b63c31d2
Thibaud <creatibofficiel@gmail.com>
2025-11-27 07:48:28 +0100

    fix: use relative paths in data-source.prod.js to force .js file loading

--- data-source.prod.js
+++ data-source.prod.js
@@ -15,8 +15,9 @@ const AppDataSource = new DataSource({
 
   // IMPORTANT : Chemins ABSOLUS en production
   // __dirname ici sera /app après Docker build
-  entities: [join(__dirname, 'dist', 'src', '**', '*.entity.js')],
-  migrations: [join(__dirname, 'dist', 'src', 'migrations', '*.js')],
+  // Force .js extension to avoid TypeScript file loading
+  entities: ['dist/src/**/*.entity.js'],
+  migrations: ['dist/src/migrations/*.js'],
 
   synchronize: false,
   logging: ['error', 'warn', 'migration'],
